48b9fd041a05ada1f4f539e49f0a71fde655a5e4
Abrahamjf <abrahamjferrer@gmail.com>
2022-10-25 22:48:14 +0000

    :hammer: se termino el proyecto

--- src/app.py
+++ src/app.py
@@ -14,12 +14,17 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.json
-    decoded_object = json.loads(todos)
-    print("Incoming request with the following body", request_body)
-    return jsonify(decoded_object)
-https://github.com/Abrahamjf/a-api-with-flask.git
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    json_text = jsonify(todos)
+    print("Incoming request with the following body", decoded_object)
+    return json_text
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
